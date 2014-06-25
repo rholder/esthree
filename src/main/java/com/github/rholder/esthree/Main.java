a9dcb55ab18b010e4f9fa8fd86670609ae602cdf
Ray Holder <ray@blacklocus.com>
2014-06-24 23:19:43 -0500

    bump version up to 0.2.1-SNAPSHOT in Main

--- src/main/java/com/github/rholder/esthree/Main.java
+++ src/main/java/com/github/rholder/esthree/Main.java
@@ -34,7 +34,7 @@ public class Main {
 
     public static String getVersion() {
         // TODO pull this from the manifest
-        return "0.2.0";
+        return "0.2.1-SNAPSHOT";
     }
 
     @SuppressWarnings("unchecked")
