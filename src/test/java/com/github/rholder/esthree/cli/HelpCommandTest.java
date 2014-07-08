756db2e6af792c4b975af58a2c8b0e4f6fe65688
Ray Holder <ray@blacklocus.com>
2014-07-07 21:34:33 -0500

    add help cli test

---
+++ src/test/java/com/github/rholder/esthree/cli/HelpCommandTest.java
@@ -0,0 +1,52 @@
+package com.github.rholder.esthree.cli;
+
+import com.github.rholder.esthree.Main;
+import io.airlift.command.ParseArgumentsUnexpectedException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelpCommandTest extends HelpCommand {
+
+    @Test
+    public void helpNone() {
+        Main main = new Main();
+        main.parseGlobalCli();
+        main.command.parse();
+        main.command.run();
+    }
+
+    @Test
+    public void help() {
+        Main main = new Main();
+        main.parseGlobalCli("help");
+        main.command.parse();
+        main.command.run();
+    }
+
+    @Test
+    public void helpGet() {
+        Main main = new Main();
+        main.parseGlobalCli("help", "get");
+        main.command.parse();
+        main.command.run();
+    }
+
+    @Test
+    public void helpSelf() {
+        Main main = new Main();
+        main.parseGlobalCli("help", "esthree");
+        main.command.parse();
+        main.command.run();
+    }
+
+    @Test
+    public void helpUnexpected() {
+        try {
+            Main main = new Main();
+            main.parseGlobalCli("potato");
+            Assert.fail("Expected ParseArgumentsUnexpectedException");
+        } catch (ParseArgumentsUnexpectedException e) {
+            Assert.assertTrue(e.getMessage().contains("Found unexpected parameters"));
+        }
+    }
+}
