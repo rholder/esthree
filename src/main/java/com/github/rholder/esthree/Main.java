643d6dc38df9542d26054a2cb6b5a8e2cbb1b6a2
Ray Holder <ray@blacklocus.com>
2015-02-03 23:07:15 -0600

    bump version up to 0.2.7-SNAPSHOT

--- src/main/java/com/github/rholder/esthree/Main.java
+++ src/main/java/com/github/rholder/esthree/Main.java
@@ -44,7 +44,7 @@ public class Main {
 
     public static String getVersion() {
         // TODO pull this from anywhere other than this hard coded spot
-        return "0.2.6";
+        return "0.2.7-SNAPSHOT";
     }
 
     @SuppressWarnings("unchecked")
